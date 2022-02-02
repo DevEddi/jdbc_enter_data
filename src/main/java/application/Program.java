67897be3c35065e8d65f7e7dba38adc81b04497d
DevEddi <scovinowork@gmail.com>
2022-02-02 18:17:42 -0300

    inserindo carl purple

--- src/main/java/application/Program.java
+++ src/main/java/application/Program.java
@@ -2,35 +2,42 @@ package application;
 
 import db.DB;
 
-import java.sql.Connection;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 
 public class Program {
     public static void main(String[] args) {
-
+        SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
         Connection conn = null;
-        Statement st = null;
-        ResultSet rs = null;
-
+        PreparedStatement st = null;
         try{
             conn = DB.getConnection();
+            st = conn.prepareStatement(
+                    "insert into seller "
+                            +"(Name, Email, BirthDate, BaseSalary, DepartmentId)"
+                            +"values"
+                            +"(?, ?, ?, ?, ?)");
+            st.setString(1, "Carl Purple");
+            st.setString(2, "carl@gmail.com");
+            st.setDate(3, new java.sql.Date(sdf.parse("22/04/1984").getTime()));
+            st.setDouble(4, 3000.00);
+            st.setInt(5, 4);
 
-            st = conn.createStatement();
-
-            rs = st.executeQuery("select * from department");
-
-            while(rs.next()){
-                System.out.println(rs.getInt("Id") + ", " + rs.getString("Name"));
-            }
+            int rowsAffected = st.executeUpdate();
+            System.out.println("Done! Rowns affected: " +rowsAffected);
         }catch(SQLException e){
             e.printStackTrace();
-        }
-        finally {
-            DB.closeResultSet(rs);
+        }catch(ParseException e){
+            e.printStackTrace();
+        }finally {
             DB.closeStatement(st);
             DB.closeConnection();
         }
+
+
+
+
+
     }
 }
